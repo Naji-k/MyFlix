26f8ba05bf5320bcd461061b23281b69f0f6015c
Naji Kanounji <n-kanounji@hotmail.com>
2023-10-20 09:30:22 +0200

    Initial Commit

---
+++ MyFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFlix
+//
+//  Created by Naji Kanounji on 10/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
